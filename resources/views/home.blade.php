066e8f7efc7c4e755748d5a5c740802b13b01c62
Nursyahmah Jamaluddin <nsyj96@gmail.com>
2021-07-12 03:12:22 -0400

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container-fluid">
+    <div class="white-box" style="background:white; text-align:center;">
+        <label for="" class="bigger-label pt-5" style="font-size: 30px;">رانچڠن ڤ & ڤ هارين دري رومه</label>
+        <div class="white-box" style="text-align:right;">
+            <div class="row p-3" style="justify-content:flex-end;">
+                <div class="col-2 border">
+                    <select name="" id="" class="form-control main-select2">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                    </select>
+                </div>
+                <div class="col-2 border"> : تاهون</div>
+            </div>
+            <div class="row p-3">
+                <div class="col-2 border">
+                    <select name="tajuk" id="" class="form-control main-select2">
+                        <option value="Tilawah"> تلاوة </option>
+                        <option value="Hafazan">حفظن</option>
+                        <option value="Tafsir">تفسير</option>
+                    </select>
+                </div>
+                <div class="col-2 border"> : تاجوق</div>
+            </div>
+            <div class="row p-3">
+                <div class="col-2 border">
+                    <input type="datetime-local" class="form-control" name="sampai" id="">
+                </div>
+                <div class="col-2 border">
+                    هيڠݢا
+                </div>
+                <div class="col-2 border">
+                    <input type="datetime-local" class="form-control" name="dari" id="">
+                </div>
+                <div class="col-2 border"> : ماس & تاريخ</div>
+            </div>
+        </div>
+
+    </div>
+</div>
+@endsection
+@section('script')
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<script>
+$(document).ready(function() {
+    $('.main-select2').select2();
+});
+</script>
+@endsection
---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,13 @@
+<!-- need to remove -->
+<li class="nav-item">
+    <a href="{{ route('home') }}" class="nav-link active">
+        <i class="nav-icon fas fa-home"></i>
+        <p>Home</p>
+    </a>
+</li>
+<li class="nav-item">
+    <a href="" class="nav-link">
+        <i class="nav-icon fas fa-home"></i>
+        <p>Pelan Pembelajaran</p>
+    </a>
+</li>
